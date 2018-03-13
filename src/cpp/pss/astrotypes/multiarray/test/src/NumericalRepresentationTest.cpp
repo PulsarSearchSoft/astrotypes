68406e51831fb2fd3210825d27b11f71f9a29ad0
Chris Williams <christopher.williams@physics.ox.ac.uk>
2018-03-13 09:04:09 +0000

    new: NumericalRepresentation meta function

---
+++ src/cpp/pss/astrotypes/multiarray/test/src/NumericalRepresentationTest.cpp
@@ -0,0 +1,71 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2018 PulsarSearchSoft
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "astrotypes/multiarray/test/NumericalRepresentationTest.h"
+
+
+namespace pss {
+namespace astrotypes {
+namespace test {
+
+
+NumericalRepresentationTest::NumericalRepresentationTest()
+    : ::testing::Test()
+{
+}
+
+NumericalRepresentationTest::~NumericalRepresentationTest()
+{
+}
+
+void NumericalRepresentationTest::SetUp()
+{
+}
+
+void NumericalRepresentationTest::TearDown()
+{
+}
+
+TEST_F(NumericalRepresentationTest, test_c_array)
+{
+    static_assert(std::is_same<typename NumericalRepresentation<bool[]>::type, bool>::value, "wrong type");
+}
+
+TEST_F(NumericalRepresentationTest, test_std_vector)
+{
+    static_assert(std::is_same<typename NumericalRepresentation<std::vector<bool>>::type, bool>::value, "wrong type");
+}
+
+TEST_F(NumericalRepresentationTest, test_std_map)
+{
+    static_assert(std::is_same<typename NumericalRepresentation<std::map<bool>>::type, bool>::value, "wrong type");
+}
+
+TEST_F(NumericalRepresentationTest, test_std_list)
+{
+    static_assert(std::is_same<typename NumericalRepresentation<std::list<bool>>::type, bool>::value, "wrong type");
+}
+
+} // namespace test
+} // namespace astrotypes
+} // namespace pss
