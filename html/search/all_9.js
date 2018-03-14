fd1ae97fd56ad3944f8f130e36203d6f2f79e3e5
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-03-14 17:23:52 +0100

    Updating documentation.

--- html/search/all_9.js
+++ html/search/all_9.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['astrotypes',['astrotypes',['../namespacepss_1_1astrotypes.html',1,'pss']]],
   ['multiarray',['multiarray',['../namespacepss_1_1astrotypes_1_1multiarray.html',1,'pss::astrotypes']]],
-  ['pointerallocator',['PointerAllocator',['../classpss_1_1astrotypes_1_1PointerAllocator.html',1,'pss::astrotypes::PointerAllocator&lt; LocalType &gt;'],['../classpss_1_1astrotypes_1_1PointerAllocator.html#ad4ed96b8f829fd3804e324408eb814c9',1,'pss::astrotypes::PointerAllocator::PointerAllocator()']]],
+  ['pointerallocator',['PointerAllocator',['../classpss_1_1astrotypes_1_1PointerAllocator.html',1,'pss::astrotypes::PointerAllocator&lt; LocalType &gt;'],['../classpss_1_1astrotypes_1_1PointerAllocator.html#a3f86bc2cf2054275bb4b9108d420e53d',1,'pss::astrotypes::PointerAllocator::PointerAllocator()']]],
   ['pointerallocator_2eh',['PointerAllocator.h',['../PointerAllocator_8h.html',1,'']]],
   ['pointerallocatortest',['PointerAllocatorTest',['../classpss_1_1astrotypes_1_1test_1_1PointerAllocatorTest.html',1,'pss::astrotypes::test::PointerAllocatorTest'],['../classpss_1_1astrotypes_1_1test_1_1PointerAllocatorTest.html#a0b34b0d63eac06bb2f83c70ab6821a97',1,'pss::astrotypes::test::PointerAllocatorTest::PointerAllocatorTest()']]],
   ['pointerallocatortest_2ecpp',['PointerAllocatorTest.cpp',['../PointerAllocatorTest_8cpp.html',1,'']]],
--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['pointerallocator',['PointerAllocator',['../classpss_1_1astrotypes_1_1PointerAllocator.html#ad4ed96b8f829fd3804e324408eb814c9',1,'pss::astrotypes::PointerAllocator']]],
+  ['pointerallocator',['PointerAllocator',['../classpss_1_1astrotypes_1_1PointerAllocator.html#a3f86bc2cf2054275bb4b9108d420e53d',1,'pss::astrotypes::PointerAllocator']]],
   ['pointerallocatortest',['PointerAllocatorTest',['../classpss_1_1astrotypes_1_1test_1_1PointerAllocatorTest.html#a0b34b0d63eac06bb2f83c70ab6821a97',1,'pss::astrotypes::test::PointerAllocatorTest']]]
 ];
