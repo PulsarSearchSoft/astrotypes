6d27d39d78edcbb8817f270d0e3b697172c17165
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-03-15 16:27:53 +0100

    Added more conversions to and from std::chrono (from cheetah).

--- src/cpp/pss/astrotypes/units/Time.h
+++ src/cpp/pss/astrotypes/units/Time.h
@@ -30,6 +30,7 @@
 #include <boost/units/systems/si/time.hpp>
 #include <boost/units/quantity.hpp>
 #pragma GCC diagnostic pop
+#include <chrono>
 
 #ifndef ASTROTYPES_UNITS_TIME_H
 #define ASTROTYPES_UNITS_TIME_H
@@ -38,22 +39,62 @@ namespace pss {
 namespace astrotypes {
 
 // Units
-using second = boost::units::si::time;
+using time = boost::units::si::time;
+using second = time;
 using seconds = second;
-using millisecond = boost::units::make_scaled_unit<boost::units::si::time, boost::units::scale<10,
-        boost::units::static_rational<-3>>>::type;
+using millisecond = boost::units::make_scaled_unit<second, boost::units::scale<10,
+    boost::units::static_rational<-3>>>::type;
 using milliseconds = millisecond;
-using microsecond = boost::units::make_scaled_unit<boost::units::si::time, boost::units::scale<10,
-        boost::units::static_rational<-6>>>::type;
+using microsecond = boost::units::make_scaled_unit<second, boost::units::scale<10,
+    boost::units::static_rational<-6>>>::type;
 using microseconds = microsecond;
-using nanosecond = boost::units::make_scaled_unit<boost::units::si::time, boost::units::scale<10,
-        boost::units::static_rational<-9>>>::type;
+using nanosecond = boost::units::make_scaled_unit<second, boost::units::scale<10,
+    boost::units::static_rational<-9>>>::type;
 using nanoseconds = nanosecond;
 
 /**
  * @brief Time dimension type.
  */
-using Time = boost::units::quantity<boost::units::si::time, double>;
+using Time = boost::units::quantity<time, double>;
+
+/**
+ * @brief Second data type.
+ */
+template<typename DataType>
+class Seconds : public boost::units::quantity<second, DataType>
+{
+    using BaseType = boost::units::quantity<second, DataType>;
+
+    /**
+     * @brief Default empty constructor.
+     */
+    Seconds() {}
+    /**
+     * @brief Conversion from other boost::units::quantity types.
+     * @param quantity boost::units::quantity object from which to convert
+     */
+    template<typename UnitType, typename OtherDataType>
+    Seconds(boost::units::quantity<UnitType, OtherDataType> const & quantity) : BaseType(quantity) {}
+    /**
+     * @brief Conversion from std::chrono::duration types
+     * @param duration std::chrono::duration from which to convert
+     */
+    template<typename DurationType, typename PeriodType>
+    explicit Seconds(std::chrono::duration<DurationType, PeriodType> const & duration)
+        : BaseType(std::chrono::duration_cast<std::chrono::duration<DurationType, std::ratio<1, 1>>>(duration).count()
+               * seconds) {}
+
+};
+
+/**
+ * @brief Mimic the std::duration_cast for boost::units::quantity
+ */
+template<typename DataType, typename DurationType, typename PeriodType>
+constexpr boost::units::quantity<time, DataType> duration_cast(const std::chrono::duration<DurationType, PeriodType> &
+    duration) {
+    return boost::units::quantity<time, DataType>(std::chrono::duration_cast<std::chrono::duration<DataType,
+        std::ration<1, 1>>>(duration).count() * seconds);
+};
 
 } // namespace astrotypes
 } // namespace pss
