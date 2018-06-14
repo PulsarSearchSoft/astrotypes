d791b2e277bd14f8164b580e7d05ee14fc282015
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-06-14 15:19:50 +0200

    Fixed the use of iterators.

--- src/cpp/pss/astrotypes/types/detail/Utilities.cpp
+++ src/cpp/pss/astrotypes/types/detail/Utilities.cpp
@@ -28,8 +28,8 @@ namespace astrotypes {
 template<typename T>
 void transpose(TimeFrequency<T> & source, FrequencyTime<T> & destination)
 {
-    for ( DimensionIndex<Time> index_time = 0; index_time < source.size<Time>(); index_time++ ) {
-        for ( DimensionIndex<Frequency> index_frequency = 0; index_frequency < source.size<Frequency>(); index_frequency++ ) {
+    for ( DimensionIndex<Time> index_time(0); index_time < source.template size<Time>(); index_time++ ) {
+        for ( DimensionIndex<Frequency> index_frequency(0); index_frequency < source.template size<Frequency>(); index_frequency++ ) {
             destination[index_frequency][index_time] = source[index_time][index_frequency];
         }
     }
@@ -38,8 +38,8 @@ void transpose(TimeFrequency<T> & source, FrequencyTime<T> & destination)
 template<typename T>
 void transpose(FrequencyTime<T> & source, TimeFrequency<T> & destination)
 {
-    for ( DimensionIndex<Frequency> index_frequency = 0; index_frequency < source.size<Frequency>(); index_frequency++ ) {
-        for ( DimensionIndex<Time> index_time = 0; index_time < source.size<Time>(); index_time++ ) {
+    for ( DimensionIndex<Frequency> index_frequency(0); index_frequency < source.template size<Frequency>(); index_frequency++ ) {
+        for ( DimensionIndex<Time> index_time(0); index_time < source.template size<Time>(); index_time++ ) {
             destination[index_time][index_frequency] = source[index_frequency][index_time];
         }
     }
