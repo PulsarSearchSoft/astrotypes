ac6d387bac3e1637da9eeb4b7f35241dd334f815
Chris Williams <christopher.williams@physics.ox.ac.uk>
2020-04-03 09:22:58 +0100

    bugfix: unit test had a memory leak

--- src/cpp/pss/astrotypes/multiarray/test/src/PointerAllocatorTest.cpp
+++ src/cpp/pss/astrotypes/multiarray/test/src/PointerAllocatorTest.cpp
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2018 PulsarSearchSoft
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -61,6 +61,7 @@ TEST(PointerAllocatorTest, test_std_vector)
     for ( std::size_t i = 0; i < size; i++ ) {
         ASSERT_EQ(i, array[i]);
     }
+    delete[] array;
 }
 
 } // namespace test
