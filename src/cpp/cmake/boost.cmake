d9aa7064068555fa479aa9302e9249a525331b1e
Mitchell Mickaliger <mitchell.mickaliger@manchester.ac.uk>
2022-10-10 15:36:22 +0000

    Updated boost.cmake to allow for use of native FindBoost.cmake variables, while keeping variables that match the new conventions

--- src/cpp/cmake/boost.cmake
+++ src/cpp/cmake/boost.cmake
@@ -14,6 +14,7 @@ if(NOT USE_THIRDPARTY_BOOST)
         find_package(Boost)
         set(BOOST_FOUND ${Boost_FOUND})
         if(BOOST_FOUND)
+            # Set some variables to match the CMake conventions found in doc/cmake_conventions.md
             set(BOOST_INCLUDE_DIRS ${Boost_INCLUDE_DIRS})
             set(BOOST_LIBRARIES ${Boost_LIBRARIES})
             set(BOOST_MAJOR_VERSION ${Boost_MAJOR_VERSION})
@@ -26,10 +27,16 @@ if(NOT USE_THIRDPARTY_BOOST)
 endif()
 
 # If no boost install is found, use the included third-party boost/units
-if(NOT BOOST_FOUND)
+if(NOT Boost_FOUND)
     message(STATUS "Using thirdparty boost.")
     # We do not use find_package(Boost ...) here, as that calls FindBoost.cmake, which requires libraries to exist, and boost::units is header-only
     set(BOOST_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/boost/1.65 CACHE PATH "" FORCE)
     set(BOOST_MAJOR_VERSION 1)
     set(BOOST_MINOR_VERSION 65)
+    # Replicate the above variables in the format output by FindBoost.cmake.
+    # These break the CMake conventions defined in this repo, but users might
+    # want to use the native format when including astrotypes in their work.
+    set(Boost_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/boost/1.65 CACHE PATH "" FORCE)
+    set(Boost_MAJOR_VERSION 1)
+    set(Boost_MINOR_VERSION 65)
 endif()
