c92765d43a7f4f225ba4c2333967c58ad2a5b3d2
DevLumbaa <devishta.lumbaa@gmail.com>
2023-11-17 20:13:50 +0000

    AT4-149 update cmake to use c++17

--- src/cpp/cmake/compiler_settings.cmake
+++ src/cpp/cmake/compiler_settings.cmake
@@ -12,6 +12,8 @@ endif()
 
 # Set compiler flags
 set(CMAKE_CXX_FLAGS "-fPIC")
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 if(CMAKE_CXX_COMPILER MATCHES icpc)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wcheck -wd2259 -wd1125")
@@ -19,7 +21,7 @@ endif()
 
 if(CMAKE_CXX_COMPILER_ID MATCHES Clang)
     set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -Werror")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -Werror")
     if(CMAKE_BUILD_TYPE MATCHES profile)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0  -fprofile-arcs -ftest-coverage")
     endif()
@@ -28,7 +30,7 @@ endif()
 if(CMAKE_COMPILER_IS_GNUCXX)
     # -Wl, --no-as-needed avoids linker problem with libfftwf3 on ubuntu systems
     set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++11 -Werror")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++17 -Werror")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wcast-align -march=native")
     if(CMAKE_BUILD_TYPE MATCHES profile)
         # -pg for gprof
